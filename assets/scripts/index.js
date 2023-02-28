402c3672a6a2ef7b1e7b0aad70518e3e100150e5
Franco Lapalma <frann.lapalma@gmail.com>
2023-02-28 16:45:27 -0300

    Task2 - Aplicación de JavaScrips para cards dinámicas

---
+++ assets/scripts/index.js
@@ -0,0 +1,27 @@
+const card = document.querySelector('#event')
+
+let cardsEvents = addCardsEvents(data.events)
+
+card.innerHTML = cardsEvents
+
+function addCardsEvents(arrayEvents) {
+    let cardsEvents = ''
+    for (const events of arrayEvents){
+        cardsEvents += `<div class="col-lg-3 col-md-6 col-sm-12 p-1">
+            <div class="card">
+                <img src="${events.image}" class="card-img-top" alt="${data.events.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${events.name}</h5>
+                    <p class="card-text">${events.description}</p>
+                    <div class="card-details">
+                        <a href="#" class="card-link">$${events.price}</a>
+                        <a href="details.html" class="btn btn-secondary">View more</a>
+                    </div>                  
+                </div>
+            </div>
+        </div>`
+    }
+    return cardsEvents 
+}
+
+ 
---
+++ assets/scripts/upcoming-events.js
@@ -0,0 +1,23 @@
+const card = document.querySelector('#event')
+
+let cardsEvents = ''
+for (const events of data.events){
+    if(data.currentDate < events.date){
+    cardsEvents += `
+    <div class="col-lg-3 col-md-6 col-sm-12 p-1" >
+        <div class="card">
+            <img src="${events.image}" class="card-img-top" alt="${events.name}">
+            <div class="card-body">
+                <h5 class="card-title">${events.name}</h5>
+                <p class="card-text">${events.description}</p>
+                <div class="card-details">
+                    <a href="#" class="card-link">$${events.price}</a>
+                    <a href="details.html" class="btn btn-secondary">View more</a>
+                </div>                  
+            </div>
+        </div>
+    </div>
+    `
+    }
+card.innerHTML = cardsEvents
+}
