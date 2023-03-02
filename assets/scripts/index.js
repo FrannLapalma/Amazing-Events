c474bfa6649a6e18b34817b120a741eff0f2cc71
Franco Lapalma <frann.lapalma@gmail.com>
2023-03-02 16:17:23 -0300

    Correción Task2 - Aplicación de filter() para upcoming events y past events

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -23,5 +23,3 @@ function addCardsEvents(arrayEvents) {
     }
     return cardsEvents 
 }
-
- 
--- assets/scripts/upcoming-events.js
+++ assets/scripts/upcoming-events.js
@@ -1,23 +1,28 @@
 const card = document.querySelector('#event')
-
-let cardsEvents = ''
-for (const events of data.events){
-    if(data.currentDate < events.date){
-    cardsEvents += `
-    <div class="col-lg-3 col-md-6 col-sm-12 p-1" >
-        <div class="card">
-            <img src="${events.image}" class="card-img-top" alt="${events.name}">
-            <div class="card-body">
-                <h5 class="card-title">${events.name}</h5>
-                <p class="card-text">${events.description}</p>
-                <div class="card-details">
-                    <a href="#" class="card-link">$${events.price}</a>
-                    <a href="details.html" class="btn btn-secondary">View more</a>
-                </div>                  
-            </div>
-        </div>
-    </div>
-    `
-    }
+let cardsEvents = addCardsUpcomingEvents(data.events)
 card.innerHTML = cardsEvents
+
+function addCardsUpcomingEvents(arrayEvents){
+    let cardsEvents = ''
+       
+    let events = arrayEvents.filter((events)=>{
+        if (events.date>data.currentDate) {
+            cardsEvents += `
+                <div class="col-lg-3 col-md-6 col-sm-12 p-1" >
+                    <div class="card">
+                        <img src="${events.image}" class="card-img-top" alt="${events.name}">
+                        <div class="card-body">
+                            <h5 class="card-title">${events.name}</h5>
+                            <p class="card-text">${events.description}</p>
+                            <div class="card-details">
+                                <a href="#" class="card-link">$${events.price}</a>
+                                <a href="details.html" class="btn btn-secondary">View more</a>
+                            </div>                  
+                        </div>
+                    </div>
+                </div>
+                `
+        }
+    })
+    return cardsEvents
 }
